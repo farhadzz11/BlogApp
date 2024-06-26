3bf38c8ecdc703d439d46d1fc15b02e879232e30
Farhad <163932633+farhadzz11@users.noreply.github.com>
2024-06-26 11:38:30 +0330

    creaste the site title

--- Header.jsx
+++ Header.jsx
@@ -8,7 +8,13 @@ const Header = () => {
     <div className='container'>
         <div className='navbar '>
           <Image src={assets.logo} width={180}  alt='' id='img'  />
-          <button id='btn'>Get Started</button>
+          <button id='btn'>Get Started <Image src={assets.arrow} /></button>
+
+        </div>
+
+        <div className='container-text'>
+          <h1 >Latest Blog</h1>
+          <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, magnam!</p>
 
         </div>
       
